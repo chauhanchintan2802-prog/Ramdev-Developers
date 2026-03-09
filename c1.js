7fae065d41ac9e7ef0ed9ebc508eb3e525fc05d4
chauhanchintan2802-prog <chauhanchintan.2802@gmail.com>
2026-03-09 11:00:48 +0530

    Add files via upload

---
+++ c1.js
@@ -0,0 +1,13 @@
+function scrollToContact() {
+
+document.getElementById("contact").scrollIntoView({
+behavior: "smooth"
+});
+
+}
+
+function sendMessage(){
+
+alert("Message sent successfully! We will contact you soon.");
+
+}
